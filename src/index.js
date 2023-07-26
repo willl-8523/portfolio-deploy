96880fae4b1729245ebece9d21528c191f926cc3
willl85 <ngowilfried@gmail.com>
2023-07-26 16:11:48 +0200

    Retirer le reactStrictMode

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import { SidebarProvider } from './contexts/Sidebar.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <SidebarProvider>
-      <App />
-    </SidebarProvider>
-  </React.StrictMode>
+  <SidebarProvider>
+    <App />
+  </SidebarProvider>
 );
 
 reportWebVitals();
