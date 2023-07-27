505b9ecea725e02b9ac1922303eeadfb07bd7f5c
willl85 <ngowilfried@gmail.com>
2023-07-27 18:34:32 +0200

    Mis a jour du fichier 404.html

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Portfolio from './pages/Portfolio.page';
 const App = () => {
   return (
     <div className="flex">
-        <Router>
+        <Router basename="/portfolio-deploy">
           <Menu />
           <main
             id="main"
